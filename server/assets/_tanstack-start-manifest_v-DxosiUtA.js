6d0c4a34168fdb6c750c6dcbc27f06e97c0892bf
DharaniManne6 <dharu.manne@gmail.com>
2026-06-06 05:06:51 -0400

    Updates

--- server/assets/_tanstack-start-manifest_v-DxosiUtA.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/dharu/Downloads/dharani_manne_portfolio_full/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BYq61leb.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BYq61leb.js" } }] }, "/": { filePath: "C:/Users/dharu/Downloads/dharani_manne_portfolio_full/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-ryD_xLxR.js"] } } });
-export {
-  tsrStartManifest
-};
